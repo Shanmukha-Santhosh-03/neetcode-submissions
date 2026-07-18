0e3019d797c28c7bca93fd2e89956651f65046bb
Shanmukha Santhosh Kadimella <shannu1606@gmail.com>
2026-07-18 15:02:15 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+       /* int init=1,end=1;
+        int []pre=new int[nums.length];
+        int []post=new int[nums.length];
+        pre[0]=init*nums[0];
+        int op[]=new int[nums.length];
+        post[nums.length-1]=nums[nums.length-1];
+        for(int i=1;i<nums.length;i++){
+            pre[i]=pre[i-1]*nums[i];
+            post[nums.length-i-1]=post[nums.length-i]*nums[nums.length-i-1];
+
+        }
+        op[0]=init*post[1];
+        for(int i=1;i<nums.length;i++){
+            if(i==0){
+                 op[i]=init*post[1];
+            }
+            else if(i==nums.length-1){
+                op[i]=end*pre[i-1];
+            }
+            else{op[i]=pre[i-1]*post[i+1];}
+        }
+        return op;*/
+        int pre=1;
+        int []op=new int[nums.length];
+        op[0]=1;
+        for(int i=1;i<nums.length;i++){
+            op[i]=pre*nums[i-1];
+            pre=op[i];
+        }
+        int post=1;
+        for(int i=nums.length-1;i>=0;i--){
+            op[i]=post*op[i];
+            
+            post*=nums[i];
+
+        }
+        return op;
+        
+
+    }
+}  
